5ebce930e825e93844617a6a90cc9154b58646a5
dlh <du__th@163.com>
2026-02-28 11:12:43 +0800

    修改

--- src/globalDefaultApps.ts
+++ src/globalDefaultApps.ts
@@ -5,8 +5,6 @@ export const blackListAppIDs: string[] = [
   'com.android.soundrecorder', // 录音机
   'com.android.email', // 电子邮件
   'com.android.deskclock', // 时钟
-  'com.duokan.phone.remotecontroller', // 万能遥控
-  'com.mfashiongallery.emag', // 小米画报
   'com.android.calendar', // 日历
   'com.android.providers.downloads.ui', // 下载管理
 
@@ -16,140 +14,25 @@ export const blackListAppIDs: string[] = [
   'com.eg.android.AlipayGphone', //支付宝
   'com.tencent.wetype', // 微信输入法
   'com.tmri.app.main', // 交管12123
-  'com.github.android', // GitHub
-  'org.telegram.messenger', // Telegram
-  'com.twitter.android', // X(推特)
-  'com.aistra.hail', // 雹
-  'com.heyanle.easybangumi4', // 纯纯看番
-  'com.perol.pixez', // PixEz
-  'com.perol.play.pixez', // PixEz Google Play版
-  'com.magicalstory.videos', // 奇妙影视
-  'com.magicalstory.installer', // 奇妙安装程序
-  'com.guozhigq.pilipala', // PiliPala
-  'com.salt.music', // Salt Player
-  'com.xuncorp.qinalt.music', // 青盐云听
-  'com.github.wgh136.pica_comic', // Pica Comic
-  'com.yenaly.han1meviewer', // Han1meViewer
-  'ru.zdevs.zarchiver', // ZArchiver
-  'ru.zdevs.zarchiver.pro', // ZArchiver Pro
-  'me.zhanghai.android.files', // 质感文件
-  'com.xjcheng.musictageditor', // 音乐标签
-  'com.lalilu.lmusic', // LMusic
-  'com.ktls.fileinfo', // 存储空间清理
-  'idm.internet.download.manager', // 1DM
-  'idm.internet.download.manager.plus', // 1DM+
-  'com.mxtech.videoplayer', // MX播放器
-  'com.mxtech.videoplayer.pro', // MX播放器专业版
   'com.github.kr328.clash', // Clash for Android
-  'com.x8bit.bitwarden', // Bitwarden
   'org.localsend.localsend_app', // LocalSend
   'moe.shizuku.privileged.api', // Shizuku
-  'com.ktls.automation', // 自动点击工具
-  'com.deepl.mobiletranslator', // DeepL
-  'com.accessibilitymanager', // 无障碍管理器
-  'me.tasy5kg.cutegif', // 小萌GIF
-  'com.absinthe.libchecker', // LibChecker
-  'com.github.metacubex.clash.meta', // Clash Meta for Android
-  'io.github.jd1378.otphelper', // 短信验证码自动复制
-  'com.valvesoftware.android.steam.community', // Steam
   'io.legado.app.release', // 阅读
-  'com.getsurfboard', // Surfboard
-  'moe.nb4a', // NekoBox
   'com.v2ray.ang', // v2rayNG
-  'com.xiaoyv.bangumi', // Bangumi for Android
-  'com.funny.translation', // 译站
-  'com.looker.droidify', // Droid-ify
-  'app.eleven.com.fastfiletransfer', // 文件闪传
-  'com.idaodan.clean.master', // 雪豹速清
-  'com.samsung.agc.gcam84', // 相机
-  'com.wsw.cospa', // 异次元
   'app.mihon', // Mihon
-  'com.rosan.installer.x', // installerX
-  'app.rikka.savecopy', // 保存副本
-  'com.rosan.dhizuku', // Dhizuku
-  'com.rosan.accounts', // Accounts
-  'com.dv.adm', // ADM
-  'com.assistant.ongoingclear', // 固定通知隐藏
-  'org.koitharu.kotatsu', // Kotatsu
-  'me.ash.reader', // Read You
-  'com.huanchengfly.tieba.post', // 贴吧Lite
-  'com.example.c001apk', // c001apk
-  'dev.anilbeesetti.nextplayer', // Next Player
-  'org.kde.kdeconnect_tp', // KDE Connect
   'mark.via', // via浏览器
   'mark.via.gp', // via浏览器Google Play版
-  'com.mmbox.xbrowser', // X浏览器
-  'com.mmbox.xbrowser.pro', // X浏览器Google Play版
-  'com.mycompany.app.soulbrowser', // soul浏览器
   'com.bilibili.app.in', // bilibili
   'com.alicloud.databox', // 阿里云盘
-  'io.nichijou.flutter.mikan', // 蜜柑计划
-  'xyz.nextalone.nagram', // Nagram
   'com.google.android.apps.authenticator2', // Authenticator
   'me.jmh.authenticatorpro', // Authenticator Pro
-  'com.openai.chatgpt', // ChatGPT
-  'com.brouken.player', // Just Player
   'com.android.chrome', // Chrome
-  'hello.litiaotiao.app', // 李跳跳
-  'com.follow.clash', // FlClash
-  'com.google.android.youtube', // YouTube
   'com.xunmeng.pinduoduo', // 拼多多
-  'com.one.my_ai', // 艾跳跳
-  'com.cnspeedtest.globalspeed', // 全球网测
-  'com.github.wgh136.venera', // venera
-  'com.hicorenational.antifraud.fork', // 国家反诈中心（伪）
-  'com.predidit.kazumi', // Kazumi
-  'com.crow.pastemanga', // PasteMangaX
-  'com.omarea.vtools', // Scene
-  'deltazero.amarok', // Amarok
-  'com.sixbugs.bujuan', // BuJuan
-  'rikka.appops', // App Ops
-  'cn.wps.moffice_eng', // WPS Office
-  'main.smart.HY', // 公交e出行
   'com.hpbr.bosszhipin', // BOSS直聘
-  'pro.easybox.open', // EasyBox
   'com.music.moment', // 音乐时刻
   'com.quark.browser', // 夸克浏览器 开屏广告误触 https://i.gkd.li/i/18221460
   'com.fongmi.android.tv', // OK影视
-  'com.github.tvbox.osc', // TVBox
-  'com.junkfood.seal', // Seal
-  'com.junkfood.seal.preview', // Seal Preview
-  'com.pikcloud.pikpak', // PikPak
-  'com.deepseek.chat', // DeepSeek
-  'bin.mt.plus', // MT管理器
-  'com.yjllq.kito', // 可拓浏览器
-  'com.example.piliplus', // PiliPlus
-  'tw.nekomimi.nekogram', // Nekogram
-  'net.afdian.afdian', // 爱发电
-  'com.algermusic.app', // AlgerMusicPlayer
-  'com.coolncoolapps.easyvideorecorder', // 背景电影录像机
-  'com.termux', // Termux
-  'com.didjdk.adbhelper', // 甲壳虫ADB助手
-  'com.qingmusic.changqing', // 青听音乐
-  'ru.tech.imageresizershrinker', // Image Toolbox
-  'com.jiaohua_browser', // JMComic2
-  'com.picacomic.fregata', // PicACG
-  'com.rosan.installer.x.revived', // installerX Revived
-  'com.zhihu.hydrogen.x', // Hydrogen
-  'org.videolan.vlc', // VLC
-  'cn.cyberIdentity.certification', // 国家网络身份认证
-  'com.ifreedomer.flix', // Flix
-  'com.bitwarden.authenticator', // Bitwarden Authenticator
-  'ai.x.grok', // Grok 全局规则误触 https://i.gkd.li/i/22884434
-  'com.huanchengfly.tieba.post.self', // 贴吧Lite
-  'com.google.android.googlequicksearchbox', // Google
-  'com.google.android.apps.bard', // Gemini
-  'com.gtxfury.flyclash.meta', // FlyClash
-  'com.appshub.liclash', // LiClash
-  'com.android.purebilibili', // BiliPai
-  'com.bytedance.android.doubaoime', // 豆包输入法
-  'com.ChillyRoom.DungeonShooter', // 元气骑士
-  'com.nyapass.loader', // NyaLoader
-  'com.appshub.bettbox', // Bettbox
-  'com.abdownloadmanager', // ABDM
-  'org.easybangumi.next', // 纯纯看番 Next
-  'org.xmsleep.app', // XMSleep
-  'io.github.stelliberty', // Stelliberty
+  'com.crirp.zhipu', // 智谱
 ];
 
 // 在应用中单独禁用某个全局规则
